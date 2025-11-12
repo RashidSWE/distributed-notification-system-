2bee5ac3769a22668bc550f6af9dd1012446df03
Amazingmercy <amaefulemercy499@gmail.com>
2025-11-12 19:23:16 +0100

    chore: update tsconfig and Docker workflow for user service

--- user-service/dist/middlewares/auth.d.ts
+++
@@ -1,3 +0,0 @@
-import type { FastifyRequest, FastifyReply } from 'fastify';
-export declare const authenticate: (req: FastifyRequest, reply: FastifyReply) => Promise<undefined>;
-//# sourceMappingURL=auth.d.ts.map
